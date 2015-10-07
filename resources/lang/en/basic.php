97d1a3b4544f5309a3de82176d1813cde725d337
Stanislovas Janonis <reflexltu@gmail.com>
2015-10-08 02:24:22 +0300

    Lang files cleanup

--- resources/lang/en/basic.php
+++ resources/lang/en/basic.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-	'lang' => 'English',
-
     'sub-title' => 'LED designer, manufacturer and installer',
     'header-btn-left' => 'Tell me more',
     'header-btn-right' => 'See projects'
-    
+
 ];
--- resources/lang/lt/basic.php
+++ resources/lang/lt/basic.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    'lang' => 'Lietuvių',
-    
     'sub-title' => 'LED projektavimas, gaminimas ir įrengimas',
     'header-btn-left' => 'Papasakokit plačiau',
     'header-btn-right' => 'Mūsų projektai'
